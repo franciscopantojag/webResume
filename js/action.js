dffb27fa7182be18c53ae162e45e6fccbd237618
Francisco Eduardo Pantoja Guillén <60531342+franciscopantojag@users.noreply.github.com>
2020-06-06 03:14:15 -0500

    NavBar Fixed

--- js/action.js
+++ js/action.js
@@ -1,29 +1,61 @@
 window.onscroll = () =>verifyResumen()
-const viewPort = {
-  height: (window.innerHeight || document.documentElement.clientHeight),
-}
-
-const navBarDim = {
-  height: document.getElementById('navBar').offsetHeight,
-}
 
 
 const verifyResumen = () =>{
-  const resumen = document.getElementById('Resumen');
-  const prop = {
-    top: resumen.getBoundingClientRect().top,
-    height:resumen.offsetHeight,
-    marginBottom: resumen.getCom
+  let viewPort = {
+    height: (window.innerHeight || document.documentElement.clientHeight),
   }
-  const verRes = prop.top>-(prop.height-navBarDim.height);
-  if(verRes)
-    {
-      document.getElementById("aboutLink").classList.add("redTitle")
-      
-    }else{
-      document.getElementById("aboutLink").classList.remove("redTitle")
-    }
+  
+  const navBar = {
+    height: document.getElementById('navBar').offsetHeight,
+  }
+  const header ={
+    height: document.getElementById("Header").offsetHeight
+  } 
+  const resumen = document.getElementById('Resumen');
+  const resumenProp = {
+      top: resumen.getBoundingClientRect().top,
+      height:resumen.offsetHeight,
+  }
+  const educacion = document.getElementById('Educacion')
+  const educacionProp={
+    top: educacion.getBoundingClientRect().top,
+    height:educacion.offsetHeight
+  }
+  const habilidades= document.getElementById('Habilidades')
+  const habilidadesProp={
+    top: habilidades.getBoundingClientRect().top,
+    height:habilidades.offsetHeight
+  }
+  const experiencia= document.getElementById('Experiencia')
+  const experienciaProp={
+    top: experiencia.getBoundingClientRect().top,
+    height:experiencia.offsetHeight
+  }
+  const extra= document.getElementById('Extra')
+  const extraProp={
+    top: extra.getBoundingClientRect().top,
+    height:extra.offsetHeight
+  }
+  if((resumenProp.top>=-(resumenProp.height-navBar.height) )&& (window.pageYOffset>header.height/2)){
+    document.getElementById("aboutLink").classList.add("redTitle")
+  }
+  if (!((resumenProp.top>-(resumenProp.height-navBar.height) )&& (window.pageYOffset>header.height/2))){
+    document.getElementById("aboutLink").classList.remove("redTitle")
+  }
+  if((!((resumenProp.top>=-(resumenProp.height-navBar.height) )))&& (educacionProp.top>-(educacionProp.height-navBar.height))){
+    document.getElementById("educationLink").classList.add("redTitle")
+  } 
+  if(!((educacionProp.top<=(navBar.height+(0.02*viewPort.height) ))&& (educacionProp.top>-(educacionProp.height-navBar.height)))){
+    document.getElementById("educationLink").classList.remove("redTitle")
+  }
+  if(!((educacionProp.top>-(educacionProp.height-navBar.height)))&&((habilidadesProp.top>-(habilidadesProp.height-navBar.height)))){
+    document.getElementById("skillsLink").classList.add("redTitle")
+  }
+  if(!(!((educacionProp.top>-(educacionProp.height-navBar.height)))&&((habilidadesProp.top>-(habilidadesProp.height-navBar.height))))){
+    document.getElementById("skillsLink").classList.remove("redTitle")
+  }
 }
 
-console.log(verifyResumen())
 
+//(educacionProp.top<=(navBar.height+(0.02*viewPort.height) )
