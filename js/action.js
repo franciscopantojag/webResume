5316dd1eb3f5a81db5dd26e0bce85acad13bec07
Francisco Eduardo Pantoja Guillén <60531342+franciscopantojag@users.noreply.github.com>
2020-06-05 22:35:26 -0500

    NavBar animation added

--- js/action.js
+++ js/action.js
@@ -1,58 +1,29 @@
-/*window.onscroll = () => scrollFunction();
-const viewportHeight = window.innerHeight;
-
-const aboutSection = document.getElementById("aboutMe");
-const aboutOffsetTop = aboutSection.offsetTop;
-const aboutLink = document.getElementById("aboutLink");
-
-const skillsSection = document.getElementById("skills");
-const skillsOffsetTop = skillsSection.offsetTop;
-const skillsLink = document.getElementById("skillsLink");
-
-const educationSection = document.getElementById("education");
-const educationOffsetTop = educationSection.offsetTop;
-const educationLink = document.getElementById("educationLink");
-
-const experienceSection = document.getElementById("experience");
-const experienceOffsetTop = experienceSection.offsetTop;
-const experienceLink = document.getElementById("experienceLink");
-
-const extraSection = document.getElementById("extra");
-const extraOffsetTop = extraSection.offsetTop;
-const extraLink = document.getElementById("extraLink");
-
-
-
-const scrollFunction = () => {
-    if (window.pageYOffset >= aboutOffsetTop - 0.7 * viewportHeight && window.pageYOffset <= educationOffsetTop - 0.2 * viewportHeight) {
-        aboutLink.classList.add("redTitle");
-      }else {
-        aboutLink.classList.remove("redTitle");
-      }
-    if (window.pageYOffset >= educationOffsetTop - 0.2 * viewportHeight && window.pageYOffset <= skillsOffsetTop - 0.2 * viewportHeight) {
-        educationLink.classList.add("redTitle");
-    } else {
-        educationLink.classList.remove("redTitle");
-    }  
-    if (window.pageYOffset >= skillsOffsetTop - 0.2 * viewportHeight && window.pageYOffset <= experienceOffsetTop - 0.2 * viewportHeight) {
-        skillsLink.classList.add("redTitle");
-    }else {
-        skillsLink.classList.remove("redTitle");
-     }
-
-
-     if (window.pageYOffset >= experienceOffsetTop - 0.2 * viewportHeight && window.pageYOffset <= extraOffsetTop - 0.2 * viewportHeight) {
-        experienceLink.classList.add("redTitle");
-      } else {
-        experienceLink.classList.remove("redTitle");
-      }
-      if (window.pageYOffset >= extraOffsetTop - 0.2 * viewportHeight) {
-        extraLink.classList.add("redTitle");
-      } else {
-        extraLink.classList.remove("redTitle");
-      }
-
-
-
+window.onscroll = () =>verifyResumen()
+const viewPort = {
+  height: (window.innerHeight || document.documentElement.clientHeight),
 }
-*/
+
+const navBarDim = {
+  height: document.getElementById('navBar').offsetHeight,
+}
+
+
+const verifyResumen = () =>{
+  const resumen = document.getElementById('Resumen');
+  const prop = {
+    top: resumen.getBoundingClientRect().top,
+    height:resumen.offsetHeight,
+    marginBottom: resumen.getCom
+  }
+  const verRes = prop.top>-(prop.height-navBarDim.height);
+  if(verRes)
+    {
+      document.getElementById("aboutLink").classList.add("redTitle")
+      
+    }else{
+      document.getElementById("aboutLink").classList.remove("redTitle")
+    }
+}
+
+console.log(verifyResumen())
+
